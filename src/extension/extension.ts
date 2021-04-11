d7491e90203d74b7ea8a69c7d26ffb05eb8bfe00
Kazuki Negoro <negokaz@gmail.com>
2021-04-12 08:47:34 +0900

    ✨feat: Zenn のドキュメントがワークスペースにあるときだけ機能を有効化

--- src/extension/extension.ts
+++ src/extension/extension.ts
@@ -7,6 +7,8 @@ import PreviewViewManager from './preview/previewViewManager';
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+    vscode.commands.executeCommand('setContext', 'zenn-editor.activated', true);
+
 	context.subscriptions.push(
 		vscode.commands.registerCommand('zenn-editor.preview', previewDocument(context)),
         vscode.commands.registerCommand('zenn-editor.open-image-uploader', openImageUploader()),
